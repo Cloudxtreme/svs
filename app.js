62ccbc2e4711a80cea4a62dd27162d371e5d5c21
Bryan Tong <contact@nullivex.com>
2014-03-05 13:03:27 -0700

    now creates file root on startup should fix travis build

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 'use strict';
 var express = require('express')
+  , fs = require('fs')
   , config = require('./config')
   , app = express()
 
 if(!config.get('apiSecret')){
   console.log('No API Secret set, refusing to start...')
   process.exit()
+}
+
+//make sure the root folder exists
+if(!fs.existsSync(config.get('root'))){
+  fs.mkdirSync(config.get('root'))
 }
 
 //import file
